49daf7935a4d681347caecbbf66dd04454fdcb56
Jim Wright <jimwright808@gmail.com>
2019-03-26 08:49:38 +0000

    Setup files to mirror first project attempt

---
+++ sr-website/src/components/NavBar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="nav" class="nav">
+    <router-link to="/" class="nav__logo">Silk Road</router-link>
+    <nav>
+      <router-link to="/about" class="nav__item">About</router-link>
+      <router-link to="/menu" class="nav__item">Menu</router-link>
+      <router-link to="/findus" class="nav__item">Find Us</router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 10rem;
+  padding: 0 4rem;
+  background-color: $color-primary;
+  font-size: 2rem;
+
+  &__logo {
+    color: $color-white;
+  }
+
+  &__item {
+    color: $color-secondary;
+    padding: 0 2rem;
+  }
+}
+</style>
---
+++ sr-website/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+
+export default {
+  name: "home",
+  components: {
+    NavBar
+  }
+};
+</script>
