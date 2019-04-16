b17d0b4455919e300f373709822b546230170ced
Jim Wright <jimwright808@gmail.com>
2019-04-16 09:45:00 +0100

    Create View Menu button

--- sr-website/src/components/Menu.vue
+++ sr-website/src/components/Menu.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="menu">
     <div class="menu__card">
-
+      <button class="menu__card-button">View Menu</button>
     </div>
   </section>
 </template>
@@ -22,10 +22,30 @@
   justify-content: center;
 
   &__card {
-    width: 70vw;
-    height: 70vh;
+    width: 50vw;
+    height: 60vh;
     background-color: $color-white; 
     border-radius: 2px;
+
+    &-button {
+      display:inline-block;
+      padding:0.46em 1.6em;
+      border:0.1em solid $color-black;
+      margin:0 0.2em 0.2em 0;
+      border-radius:0.12em;
+      box-sizing: border-box;
+      text-decoration:none;
+      font-family:'Roboto',sans-serif;
+      font-weight:300;
+      font-size: 2.4rem;
+      color:$color-black;
+      text-align:center;
+
+      &:hover {
+        color: $color-white;
+        background-color: $color-black;
+      }
+    }
   }
 }
 
