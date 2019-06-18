e439dd427cd4b723224a8580ba276635982f0403
Jim Wright <jimwright808@gmail.com>
2019-06-18 10:03:17 +0100

    Add more menu items to cold component

--- sr-website/src/components/MenuCold.vue
+++ sr-website/src/components/MenuCold.vue
@@ -12,7 +12,7 @@
           {
             name: 'Mix Shreds Salad',
             veg: true,
-            desc: 'Carrot vermicelli & spinach in a hot & sour sauce',
+            desc: 'Carrot vermicelli & spinach in hot & sour sauce',
             price: 3
           },
           {
@@ -20,6 +20,36 @@
             veg: true,
             desc: 'Onion, green chilli, tomato & coriander',
             price: 3
+          },
+          {
+            name: 'Cucumber in Sauce',
+            veg: true,
+            desc: 'Cucumber cubes in special sauce',
+            price: 3
+          },
+          {
+            name: 'Shredded Kelp',
+            veg: true,
+            desc: 'Shredded kelp in hot sauce',
+            price: 3
+          },
+          {
+            name: 'Pork with Garlic Sauce',
+            veg: false,
+            desc: 'Sliced belly pork in hot sauce',
+            price: 4
+          },
+          {
+            name: 'Assorted Spiced Beef',
+            veg: false,
+            desc: 'Sliced spiced beef in a special sauce with chilli oil',
+            price: 4
+          },
+          {
+            name: 'Ox Tripe in Chilli Sauce',
+            veg: false,
+            desc: 'Shredded tripe in a tasty chilli sauce',
+            price: 4
           },
         ]
       }
