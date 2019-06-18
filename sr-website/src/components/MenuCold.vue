fbd3d4638c9fc47779b72dc32bee8452c6058f1e
Jim Wright <jimwright808@gmail.com>
2019-06-18 09:51:35 +0100

    Start moving menu display into separate components

---
+++ sr-website/src/components/MenuCold.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        cold: [
+          {
+            name: 'Mix Shreds Salad',
+            veg: true,
+            desc: 'Carrot vermicelli & spinach in a hot & sour sauce',
+            price: 3
+          },
+          {
+            name: 'Xinjiang Spicy Salad',
+            veg: true,
+            desc: 'Onion, green chilli, tomato & coriander',
+            price: 3
+          },
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
